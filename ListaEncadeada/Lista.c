31f8c950de6b59c3d261c6e69bfb96a9954d76eb
Udesc <udesc@udesc-HP-Compaq-6005-Pro-MT-PC>
2014-10-08 22:44:21 -0300

    Added new Functions

--- ListaEncadeada/Lista.c
+++ ListaEncadeada/Lista.c
@@ -10,19 +10,169 @@ void inicializa_lista(Lista *l, size_t t)
 
 int insereNoInicio(Lista *l, void *info)
 {
-    Elemento *p = (Elemento*) malloc(sizeof(Elemento));
-    if(p == NULL)
-        return ERRO_ALOCACAO;
-    p->info = malloc(l->tamInfo);
-    if(p->info == NULL)
-    {
-        free(p);
-        return ERRO_ALOCACAO;
-    }
-    memcpy(p->info, info, l->tamInfo);
+    Elemento *p = aloca_elemento(l->tamInfo, info);
     p->proximo = l->cabeca;
     l->cabeca = p;
     return 1; //Sucesso
+}
+
+int removeDoInicio(Lista *l, void *info)
+{
+    if (l->cabeca == NULL)
+    {
+        return ERRO_LISTA_VAZIA;
+    }
+    Elemento *p = l->cabeca;
+    l->cabeca = p->proximo;
+    memcpy(info, p->info, l->tamInfo);
+    free(p->info);
+    free(p);
+    return 1; //Sucesso
+}
+
+int insereNoFim(Lista *l, void *info)
+{
+    if(l->cabeca == NULL)
+    {
+        return insereNoInicio(l, info);
+    }
+
+    Elemento *aux = l->cabeca;
+    while(aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+    }
+
+    Elemento *p = aloca_elemento(l->tamInfo, info);
+    p->proximo = NULL;
+    aux->proximo = p;
+    return 1; //Sucesso
+}
+
+int removeDoFim(Lista *l, void *info)
+{
+    if (l->cabeca == NULL)
+        return ERRO_LISTA_VAZIA;
+
+    if(l->cabeca->proximo == NULL)
+        return removeDoInicio(l, info);
+
+    Elemento *aux = l->cabeca;
+    while(aux->proximo->proximo != NULL)
+    {
+        aux = aux->proximo;
+    }
+    memcpy(info, aux->proximo->info, l->tamInfo);
+    free(aux->proximo->info);
+    free(aux->proximo);
+    aux->proximo = NULL;
+    return 1; //Sucesso
+}
+
+int insereNaPosicao(Lista *l, void *info, int pos)
+{
+    if(pos < 0)
+        return ERRO_POSICAO_INVALIDA;
+    if(pos == 0)
+        return insereNoInicio(l, info);
+
+    Elemento *aux = l->cabeca;
+    int cont = 0;
+    while(cont < pos-1 && aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+        cont++;
+    }
+    if(cont != pos-1)
+        return ERRO_POSICAO_INVALIDA;
+
+    Elemento *novo = aloca_elemento(l->tamInfo, info);
+    if(novo == NULL)
+        return 0; //Fracasso
+    novo->proximo = aux->proximo;
+    aux->proximo = novo;
+    return 1; //Sucesso
+}
+
+int removeNaPosicao(Lista *l, void *info, int pos)
+{
+    if(pos < 0)
+        return ERRO_POSICAO_INVALIDA;
+    if(pos == 0)
+        return removeDoInicio(l, info);
+
+    Elemento *aux = l->cabeca;
+    int cont = 0;
+    while(cont < pos-1 && aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+        cont++;
+    }
+    if (cont != pos-1 || aux->proximo == NULL)
+        return ERRO_POSICAO_INVALIDA;
+
+    memcpy(info, aux->proximo->info, l->tamInfo);
+
+    Elemento *aux2 = aux->proximo;
+    aux->proximo = aux->proximo->proximo;
+
+    free(aux2->info);
+    free(aux2);
+    return 1; //Sucesso
+}
+
+int modificaNaPosicao(Lista *l, void *info, int pos)
+{
+    if(pos < 0)
+        return ERRO_POSICAO_INVALIDA;
+
+    Elemento *aux = l->cabeca;
+    int cont = 0;
+    while(cont != pos && aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+        cont++;
+    }
+    if(cont != pos)
+        return ERRO_POSICAO_INVALIDA;
+    memcpy(aux->info, info, l->tamInfo);
+    return 1; //Sucesso
+}
+
+int leNaPosicao(Lista *l, void *info, int pos)
+{
+    if(pos < 0)
+        return ERRO_POSICAO_INVALIDA;
+
+    Elemento *aux = l->cabeca;
+    int cont = 0;
+    while(cont != pos && aux->proximo != NULL)
+    {
+        aux = aux->proximo;
+        cont++;
+    }
+    if(cont != pos)
+        return ERRO_POSICAO_INVALIDA;
+    memcpy(info, aux->info, l->tamInfo);
+    return 1; //Sucesso
+}
+
+Elemento *aloca_elemento(size_t t, void *info)
+{
+    Elemento *p;
+    p = (Elemento*) malloc(sizeof(Elemento));
+
+    if(p == NULL)
+        return NULL;
+
+    p->info = malloc(t);
+    if (p->info == NULL)
+    {
+        free(p);
+        return NULL;
+    }
+    memcpy(p->info, info, t);
+    return p;
 }
 
 void mostra_lista(Lista l, void (*mostra_info)(void *))
@@ -34,7 +184,7 @@ void mostra_lista(Lista l, void (*mostra_info)(void *))
     int cont = 0;
     while(aux != NULL)
     {
-        printf("%d: ", ++cont);
+        printf("%d: ", cont++);
         mostra_float(aux->info);
         aux = aux->proximo;
     }
--- ListaEncadeada/main.c
+++ ListaEncadeada/main.c
@@ -8,17 +8,32 @@ int main()
 {
     Lista l1;
     inicializa_lista(&l1, sizeof(float));
-    float x = 5.0;
-    insereNoInicio(&l1, &x);
-    x = 3.0;
+    float x = 7.0, y;
     insereNoInicio(&l1, &x);
     x = 2.0;
     insereNoInicio(&l1, &x);
+    x = 3.0;
+    insereNoInicio(&l1, &x);
+    x = 4.0;
+    insereNoFim(&l1, &x);
+    x = 5.0;
+    insereNoFim(&l1, &x);
+    insereNaPosicao(&l1, &x, 1);
+    removeDoInicio(&l1, &y);
+    removeDoFim(&l1, &y);
     mostra_lista(l1, mostra_float);
+    x = 20.0;
+    modificaNaPosicao(&l1, &x, 0);
+    printf("\n");
+    removeNaPosicao(&l1, &x, 4);
+    mostra_lista(l1, mostra_float);
+    printf("%f\n", x);
+    leNaPosicao(&l1, &x, 5);
+    printf("%f\n", x);
     return 0;
 }
 
-void mostra_float(void *info) 
+void mostra_float(void *info)
 {
     float *p = (float*)info;
     printf("%f\n", *p);
